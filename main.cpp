b92563e56e7c7696e1298ba9cf10e5fe01de2795
Georgi Dunchev <georgi.dunchev@gmail.com>
2013-01-09 23:50:28 +0200

    First commit

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ void myMessageOutput(QtMsgType type, const char *msg)
 //    case QtFatalMsg:
 //	fprintf(stderr, "Fatal: %s\n", msg);
 //	abort();
-//    }123
+//    }
 }
 
 int main(int argc, char *argv[])
