d4e01ede5dcf92b27f2caaf195e553578efd00af
Georgi Dunchev <georgi.dunchev@gmail.com>
2013-03-18 00:31:23 +0200

    push commits

--- settings.h
+++ settings.h
@@ -7,7 +7,7 @@ static const float k_fSMALL = 1e-9;
 static const float k_fMIN = -k_fMAX;
 
 //number of threads
-static const int k_nTHREADS = 8;
+static const int k_nTHREADS = 4;
 
 static const bool k_bUSE_KDTREE = false;
 
