2b071566b773428aef133c0d6a5d7e47159107a8
Georgi Dunchev <georgi.dunchev@gmail.com>
2013-07-26 01:24:46 +0300

    - reworking KDTree to worki with subdivided triangles

--- kdtreenode.h
+++ kdtreenode.h
@@ -7,22 +7,27 @@
 #include <ray.h>
 #include <intersactioninfo.h>
 #include <vector>
+#include "SubTriangle.h"
 
 class CMesh;
 
 class CKDTreeNode
 {
 public:
-    CKDTreeNode(std::vector<int>* pTriangles, int nLevel, const CAABox& BoundingBox);
+    CKDTreeNode(std::vector<CSubTriangle*>* pTriangles, int nLevel, const CAABox& BoundingBox);
     ~CKDTreeNode();
 
     void Process();
-    bool Separate(std::vector<int>& AllTriangles,  std::vector<int>* pLeftTriangleIndeces,  std::vector<int>* pRightTriangleIndeces, EDimiensions eDimension, float& fBestDivide);
+    bool Separate(std::vector<CSubTriangle*>& AllTriangles,
+					std::vector<CSubTriangle*>* pLeftTriangleIndeces,
+					std::vector<CSubTriangle*>* pRightTriangleIndeces,
+					EDimiensions eDimension,
+					float& fBestPortion);
 
     bool Intersect(const CRay& ray, CIntersactionInfo& intersectionInfo );
 private:
     CMesh &GetMesh();
-    std::vector<int>* m_pTriangles;
+    std::vector<CSubTriangle*>* m_pTriangles;
     int m_nLevel;
     CKDTreeNode * m_pLeftNode;
     CKDTreeNode * m_pRightNode;
